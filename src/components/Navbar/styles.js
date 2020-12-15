fe6b5ac992ba85c21655415616aa65c39f76b6ad
Paul Okello <paulokello273@gmail.com>
2020-12-15 07:15:38 +0300

    NavBar Styling

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    textAlign: "center",
+    marginBottom: "30px",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
